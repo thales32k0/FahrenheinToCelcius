dd656c402434bf9cb8b08ebf62ad3f8296d2db2c
Thales Eduardo <89024257+thales32k0@users.noreply.github.com>
2023-10-19 14:00:45 -0300

    Update FahrenheintToCelcius.java
    Translating from portuguese to english

--- src/FahrenheintToCelcius.java
+++ src/FahrenheintToCelcius.java
@@ -4,15 +4,31 @@ public class FahrenheitToCelsius {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Peça ao usuário para inserir a temperatura em graus Fahrenheit
-        System.out.print("Digite a temperatura em graus Fahrenheit: ");
-        double fahrenheit = scanner.nextDouble();
+        while (true) {
+            try {
+                // Ask the user to enter a temperature in Celsius Fahrenheit
+                System.out.print("Enter the temperature in degrees fahrenheint (or type 'q' to exit): ");
+                String input = scanner.next();
 
+                if (input.equalsIgnoreCase("q")) {
+                    System.out.println("Leaving the programing.");
+                    break;
+                }
+
+                double fahrenheit = Double.parseDouble(input);
+
+                // Convert from Fahrenheit to Celsius using the formula (F - 32) * 5/9
+                double celsius = (fahrenheit - 32) * 5.0 / 9.0;
+
+                // Display the result
+                System.out.println("The temperature in degrees Celsius is: " + celsius);
+            } catch (NumberFormatException e) {
+                System.err.println("Invalid input. Please, Enter a valid number.");
+            }
+        }
+
+        // Close the scanner object
         scanner.close();
-
-        // Converta de Fahrenheit para Celsius usando a fórmula (F - 32) * 5/9
-        double celsius = (fahrenheit - 32) * 5.0 / 9.0;
-
-        System.out.println("A temperatura em graus Celsius é: " + celsius);
     }
 }
+
